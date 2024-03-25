e779277ea3bfd89bc6882fda122f6e8265232dea
Ankit-can-ctrl <128883341+Ankit-can-ctrl@users.noreply.github.com>
2024-03-25 17:28:33 +0530

    setting app layout

---
+++ src/components/CityList.jsx
@@ -0,0 +1,7 @@
+import styles from "../components/CityList.module.css";
+
+function CityList() {
+  return <ul className={styles.cityList}>List</ul>;
+}
+
+export default CityList;
---
+++ src/pages/AppLayout.jsx
@@ -0,0 +1,14 @@
+import "../pages/AppLayout.module.css";
+import Sidebar from "../components/Sidebar";
+import styles from "../pages/AppLayout.module.css";
+import Map from "../components/Map";
+function AppLayout() {
+  return (
+    <div className={styles.app}>
+      <Sidebar />
+      <Map />
+    </div>
+  );
+}
+
+export default AppLayout;
